7b89e188e7c93e512bd68d2d295a2db1c16cfd85
AnujLo1c <anujlowanshi3@gmail.com>
2023-12-29 21:34:07 +0530

    day 2

--- Pattern3.java
+++ Pattern3.java
@@ -2,13 +2,14 @@ import java.util.Scanner;
 
 public class Pattern3{
     public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        for(int i=0;i<n;i++){
-            for(int j=0;j<i+1;j++){
-                System.out.print(j+1+" ");
+        try (Scanner sc = new Scanner(System.in)) {
+            int n=sc.nextInt();
+            for(int i=0;i<n;i++){
+                for(int j=0;j<i+1;j++){
+                    System.out.print(j+1+" ");
+                }
+                System.out.println();
             }
-            System.out.println();
         }
     }
 }
